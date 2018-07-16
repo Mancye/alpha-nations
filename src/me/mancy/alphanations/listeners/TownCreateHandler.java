b9dafaabbf7eef4a9f68655d4225056a75798f0e
Mandrew1 <ajcarras@me.com>
2018-07-15 22:17:30 -0400

    mac push

--- src/me/mancy/alphanations/listeners/TownCreateHandler.java
+++ src/me/mancy/alphanations/listeners/TownCreateHandler.java
@@ -1,9 +1,38 @@
 package me.mancy.alphanations.listeners;
 
+import com.palmergames.bukkit.towny.event.NewTownEvent;
+import com.palmergames.bukkit.towny.object.Town;
+import me.mancy.alphanations.main.Main;
+import me.mancy.alphanations.main.Nation;
+import me.mancy.alphanations.managers.NationManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
 public class TownCreateHandler {
 
     /*
     When a new town is created, automatically add to creator's nation
      */
+
+
+    public TownCreateHandler(Main main) {
+        main.getServer().getPluginManager().registerEvents(this, main);
+    }
+
+
+    @EventHandler
+    private void onTownCreate(NewTownEvent event) {
+        Town town = event.getTown();
+
+        if (p != null && ((Player) event.getTown().getMayor()) != null) {
+            Player p = (Player) event.getTown().getMayor();
+            Nation nation = NationManager.getPlayersNation(p);
+
+            nation.addTown(town);
+
+        }
+
+
+    }
 
 }
